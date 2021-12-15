91443cb5ad48035421f6a10537c64e247b799888
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2021-12-14 16:09:14 -0800

    adds stateprovider functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Button } from "@mui/material";
 import { auth, provider } from "../firebase";
+
 function Login() {
   const signIn = () => {
     auth
